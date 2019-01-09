f70c0c89b73ab20a25ba1846586f3b3360aa8db0
saaralonbarkat <saaralonbarkat@users.noreply.github.com>
2019-01-09 09:53:26 +0100

    Add files via upload

--- transparency register sample_dm_01.R
+++ transparency register sample_dm_01.R
@@ -1,22 +1,27 @@
 library(tidyverse)
 library(translateR)
 library(googleLanguageR)
-gl_auth("google_api.json")
+gl_auth("C:/SAAR/UNIVERSITY/R/eufood/sample building/code/google_api.json")
 library(cld2)
 library(jsonlite)
 
-raw_sample <- read.csv("raw_sample.csv") 
-
+raw_sample <- read_csv("C:/SAAR/UNIVERSITY/R/eufood/sample building/data/transparency register/raw_sample_xx.csv") 
+xx <- read.csv("C:/SAAR/UNIVERSITY/R/eufood/sample building/data/transparency register/raw_sample_xx.csv")
 
 #cleaning the text a bit 
-sample_00 <- read.csv("raw_sample.csv") %>% 
-  select(id = Identification.number.,
+colnames(raw_sample) <- colnames(xx)
+
+sample_00 <- raw_sample
+
+
+
+sample_00 <- sample_00 %>% 
+  select(id = 1,
          name = X.Organisation..name,
          fields = Fields.of.interest,
          goals = Goals...remit,
          initiatives = EU.initiatives) %>%
   mutate(info = tolower(paste(goals,initiatives)) %>% str_replace_all("\r|\n",""))
-
 
 
 #Detect the language of the text: 
@@ -33,27 +38,27 @@ df <- sample_00 %>%
   select(id,info) 
 
 #translate this subset
-##t1 <- gl_translate(df$info,target = "en")[,1]
-load("translation")
-t1 <- df$x1
-class(t1)
-t2 <- as.data.frame(t1)
-class(t2)
-#t2 <- as.data.frame(flatten(t1))
+##translation.english <- gl_translate(df$info,target = "en")[,1]
+##save(translation.english,file="translation")
 
-df2$info.en <- t2[,1]
+load("C:/SAAR/UNIVERSITY/R/eufood/sample building/temp/translation")
+
+t1 <- translation.english[,1] %>% flatten() %>% data.frame()
+
+df$info.en <- t1$translatedText
 
 
 #join this to the main dataset
-sample_01 <- left_join(sample_00,df2) %>% 
-mutate(info.en.1 = ifelse(language=="en",info,info.en))
-
-sample_01 <- sample_01 %>% 
+sample_01 <- sample_00 %>% 
+  left_join(df) %>% 
+mutate(info.en.1 = ifelse(language=="en",info,info.en)) %>% 
   mutate(url = raw_sample$Website.address.,
          person.head.name = raw_sample$Person.with.legal.responsibility,
          person.head.descr = raw_sample$Position.,
          person.eu.name = raw_sample$Person.in.charge.of.EU.relations,
-         person.eu.descr = raw_sample$Position..1)
+         person.eu.descr = raw_sample$Position..1,
+         organization.section = raw_sample$Section,
+         organization.subsection = raw_sample$Subsection)
 
 
-write.cs(sample_01,file="C:/SAAR/UNIVERSITY/R/eufood/sample building/data/transparency register/sample_food_en.csv")
+write.csv(sample_01,file="C:/SAAR/UNIVERSITY/R/eufood/sample building/data/transparency register/sample_food_en.csv")
